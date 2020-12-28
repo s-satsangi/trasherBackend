b2330180317569374705558dd5111eb7d25e7fae
jrshort89 <jrshort89@gmail.com>
2020-12-28 18:44:17 -0500

    create like

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,4 +3,21 @@ class LikesController < ApplicationController
         @likes = Like.all
         render json: @likes
     end
+
+    def create
+        if (params['like']['upvote_type'] == 'Post')
+            type = Post
+        else
+            type = Comment
+        end
+        @like = Like.new(like_params)
+        puts @like.save
+        render json: @like
+    end
+
+    private
+
+    def like_params
+        params.require(:like).permit(:user_id, :upvote_id, :upvote_type)
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
 
     def index
-        @posts = Post.all
-        render json: @posts
+        @posts = Post.all.slice(params[:num].to_i, params[:num].to_i + 10)
+        render json: @posts.to_json(include: [:comments, :likes])
+    end
+
+    def show
+        @post = Post.find(params[:id])
+        render json: @post
     end
 end
