be8473506a012d0cea9d62f05d79de9debb889c9
jrshort89 <jrshort89@gmail.com>
2021-01-02 19:39:47 -0500

    likes and post controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,6 @@ class LikesController < ApplicationController
         @likes = Like.all
         render json: @likes
     end
-<<<<<<< HEAD
 
     def create
         if (params['like']['upvote_type'] == 'Post')
@@ -23,6 +22,4 @@ class LikesController < ApplicationController
     def like_params
         params.require(:like).permit(:user_id, :upvote_id, :upvote_type)
     end
-=======
->>>>>>> 82818f7ae472365afee32fbf09ad22f8ec700518
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,6 @@ class PostsController < ApplicationController
 
     def index
         @posts = Post.all
-<<<<<<< HEAD
         # .slice(params[:num].to_i, params[:num].to_i + 10)
         render json: @posts.to_json(include: [:comments, :likes])
     end
@@ -10,8 +9,6 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
         render json: @post
-=======
         render json: @posts
->>>>>>> 82818f7ae472365afee32fbf09ad22f8ec700518
     end
 end
