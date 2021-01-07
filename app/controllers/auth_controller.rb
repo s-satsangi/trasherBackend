cdbf466a70001f93b70d18e06b3ee80bb4e6a9ba
jrshort89 <jrshort89@gmail.com>
2021-01-07 11:12:14 -0500

    login and alerts

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -2,14 +2,14 @@ class AuthController < ApplicationController
     skip_before_action :authenticate_user, only: [:create]
 
     def create
-        # byebug
         @user = User.find_by(username: user_login_params[:username])
+        # byebug
         if @user && @user.authenticate(user_login_params[:password])
             token=encode_jwt({user_id: @user.id, username: @username})
             cookies.signed[:jwt] = {value:  token, httponly: true}
             render json: {user: @user.username}, status: :accepted
         else
-            render json: { message: "Invalid username or password"}, status: :unauthorized
+            render json: {message: "Invalid username or password"}, :status => :unauthorized
          end
     end
 
