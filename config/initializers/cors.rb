7d9aed3e5dadc8903e551aca06638b0be256937d
s-satsangi <s.satsangi@gmail.com>
2021-03-11 22:29:27 -0500

    more cors fun

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -12,6 +12,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      Access-Control-Allow-Origin: true,
       credentials: false
   end
 end
