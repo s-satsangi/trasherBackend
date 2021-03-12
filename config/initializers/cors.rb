e9ab3e0b157bdadc176e3c5aa0d1097580064393
s-satsangi <s.satsangi@gmail.com>
2021-03-11 22:17:49 -0500

    cors details

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'trasher-frontend.netlify.app'
 
     resource '*',
       headers: :any,
